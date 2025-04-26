421b7275aaa329b0ed437eea5419f213f10a0500
PassimElectus <kyle.a.lobato@gmail.com>
2025-04-25 18:54:23 -0700

    commited at 6:54pm;

--- main.hpp
+++ main.hpp
@@ -1,3 +1,17 @@
 /***************************************************
  * Code your program here
  ***************************************************/
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+void readFile(int);
+void writefile(int);
+
+
+void readFile(int filename)
+{
+    ifstream ifs;
+
+}
